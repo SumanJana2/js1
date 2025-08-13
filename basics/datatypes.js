0b755e1f8dd03c662c22972bf2988170d8857ff3
SumanJana2 <420dominicontop@gmail.com>
2025-08-13 05:49:17 +0000

    xd4

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -1,3 +1,17 @@
 "use strict";//treat all js code AS NEWER VERSION //PURANE KEYWORDS KO BHI NAYA KEYWORDS KI TARAHA TREAT KARO
 //alert(3+3)//// yeh wala syntax sirf aur sirf hum browser ke console mein use kartey hein iske liye alaga syntax hein node mein 
+// code readability is important pls use indendation even if its not required for proper readablity 
+
+let name = "suman" //string("")
+let age  =18 //number (int and bigint)
+let isloggedin = false //boolean(true/false)
+//null  is a standalone value (empty means null)(typeof = object)
+//undefined =>(value has not been decided)(typeof = undefined)
+//symbol => unique emojis/characters
+
+console.log(typeof undefined) //op = undefined
+console.log(typeof null) //op = object
+//typeof shows the datatype of the variable/values
+
+
 
