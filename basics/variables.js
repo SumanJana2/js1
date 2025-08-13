278a2056feca3a66af97bdc8dd64d57e0090b446
SumanJana2 <420dominicontop@gmail.com>
2025-08-13 05:24:24 +0000

    xd2

---
+++ basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 144232
+let accountEmail = "hitesh@google.com"
+var accPas = "12345"
+accountCity = "jaipur"
+let accountstate//undefined
+
+
+//accountId = 12454454351
+/*there is problem with var keyword bcoz of block scope and fucntional scope thats why use 
+let keyword 
+account city = "jaipur" is a loosly typed isko bhi use mat kar */
+console.log(accountId)
+accountEmail = "gdggdahy@.com"
+accPass = "36423423463"
+accountCity = "begsgbhdbeu"
+
+console.table([accountId,accPass,accountEmail,accountstate])
