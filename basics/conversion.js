73c409b3f030763881e6dd4947b1949263f8d3bb
SumanJana2 <420dominicontop@gmail.com>
2025-08-13 08:58:43 +0000

    xd5

--- basics/conversion.js
+++ basics/conversion.js
@@ -1,4 +1,4 @@
-let score = "33abc"
+/*let score = "33abc"
 console.log(typeof score)
 console.log(typeof(score))
 
@@ -10,10 +10,44 @@ let score1 = null
 let score2 = undefined
 
 let valueinn = Number(score1)
-let valueinn1 = Number(score1)
+let valueinn1 = Number(score2)
 
 console.log(typeof score1)//type - > object
 console.log(valueinn)//the op will be 0 //(basic conversion from boolean to int true ->1 //false ->0))
 console.log(typeof score2)// type -> undefined 
 console.log(valueinn1)//op will be Nan
+//------------------------------------------------------------------------------------------------
+//"33" => 33 /* to number conversion*/
+//"33abc"=>Nan
+//true=> 1
+//false=>0
+//null(object)=> 0
+//undefined(undefined)=> Nan       */
+//-----------------------------------------------------------------------------------------------------------
+/*                      // To boolean conversion
+let isLogged = 1
+let booleanisLogged = Boolean(isLogged)
+console.log(booleanisLogged) //1 => true & 0 => false
+
+let isLogged1 = "hitesh"
+let booleanisLogged1 = Boolean(isLogged1)
+console.log(booleanisLogged1) //string => true & empty string=> false
+
+let isLogged2 = ""
+let booleanisLogged2 = Boolean(isLogged2)
+console.log(booleanisLogged2)
+*/
+//-----------------------------------------------------------------
+/*
+let s = 33      //To String
+let st = String(s)
+console.log(st)
+console.log(typeof st) // true => "true" & 33=>"33"
+
+let s1 = true
+let st1 = String(s1)
+console.log(st1)
+console.log(typeof st1) */
+///-------------------------------------------------------------------------
+
 
