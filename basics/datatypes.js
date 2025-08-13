6cbeb9ac89447dca878bbc819aa18a540c478c3a
SumanJana2 <420dominicontop@gmail.com>
2025-08-13 05:36:12 +0000

    xd3

---
+++ basics/datatypes.js
@@ -0,0 +1,3 @@
+"use strict";//treat all js code AS NEWER VERSION //PURANE KEYWORDS KO BHI NAYA KEYWORDS KI TARAHA TREAT KARO
+//alert(3+3)//// yeh wala syntax sirf aur sirf hum browser ke console mein use kartey hein iske liye alaga syntax hein node mein 
+
