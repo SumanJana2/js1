8a128f3f3e5ed66454cf9b0ee5590f2b5aa793eb
SumanJana2 <420dominicontop@gmail.com>
2025-08-15 20:19:48 +0000

    xd

--- basics/iife.js
+++ basics/iife.js
@@ -1,11 +1,21 @@
 // Immediately Invoked Function Expressions (IIFE)
-
+// sometimes global scope creates a pollution so wee need to remove some pollution(declarations)
+//
 
 (function chai(){
     // named IIFE
-    console.log(`DB CONNECTED`);
-})();
+    console.log(`DB CONNECTED`); //op--> DB CONNECTED
+})();//--------------------------->syntax --> ()();(first bracket mein function declaration hoga
+// yaha semicolon dalna is very important  
+// agar nhi karogey toh error ayega machine ko pata 
+// nhi chalega ki fucntion ka end kahan hein)
 
-( (name) => {
-    console.log(`DB CONNECTED TWO ${name}`);
-} )('hitesh')
+//*********************************************************************************** */
+( () => {       //unamed iife
+    console.log(`DB CONNECTED TWO`); //op-->DB CONNECTED TWO
+} )();
+//arrow function with no arguments
+//**************************************************************************************** */
+( (name) => { //unamed iife
+    console.log(`DB CONNECTED TWO ${name}`);//DB CONNECTED TWO hitesh
+} )('hitesh') //arrow fucntion with name as an arguments
