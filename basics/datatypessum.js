1b366c16f4a906a18f6374dec09839d625e268e5
SumanJana2 <420dominicontop@gmail.com>
2025-08-15 06:05:06 +0000

    xd

---
+++ basics/datatypessum.js
@@ -0,0 +1,63 @@
+//primitive datatypes (7types)(call by value--sidha value milega)
+/*-->String
+-->undefined
+-->null
+-->boolean
+-->number
+-->symbol(unquie banane ke liye hota hein)
+-->Bigint(scientific value)
+ */
+
+
+//Reference(Non primitive)(call by refernece ---sidha address milega)
+/*-->array
+-->objects
+-->functions
+ */
+//-------------------------------
+/*
+const score =100 //number
+const scorv=100.5 //number
+const isloggedin= false //boolean
+const issjnn = null //null is type of object
+let usermail;//undefined
+const sgdg= 35263264263642342434264342n//big int
+const id = Symbol('123')//symbol
+const fhyfy= Symbol('123')//symbol
+console.log(id === fhyfy)// op--> false bcoz even though 
+// the value is same but the symbol keyword makes it unquie 
+
+
+const heros= ["sharktimAN","NAAGRAJ"]///array is also a type of oject
+let obj={
+    name:"bfhfbhsd",
+    age : 45   //////////object
+}
+
+const myfunction = function(){ //my funtion is function name and we use funtion keyword 
+    console.log("hello")
+}
+
+console.log(typeof myfunction)          
+console.log(typeof obj)
+console.log(typeof heros)
+console.log(typeof id)
+console.log(typeof sgdg)
+console.log(typeof usermail)
+console.log(typeof issjnn)
+console.log(typeof scorv)
+console.log(typeof score)
+console.log(typeof isloggedin)
+*/
+/*------op----------- 
+function
+object 
+object
+symbol
+bigint
+undefined
+object
+number
+number
+boolean*/
+
