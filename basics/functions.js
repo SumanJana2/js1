64e84e34dec74bd0696df65f7d8d4e003a03bda3
SumanJana2 <420dominicontop@gmail.com>
2025-08-15 15:26:39 +0000

    xd

--- basics/functions.js
+++ basics/functions.js
@@ -1,5 +1,5 @@
-
-function sayMyName(){
+/*
+function sayMyName(){ //function definition
     console.log("H");
     console.log("I");
     console.log("T");
@@ -7,14 +7,38 @@ function sayMyName(){
     console.log("S");
     console.log("H");
 }
+// op--->
+H
+I
+T
+E
+S
+H */
+//sayMyName()//funtion call here sayMyName is a reference and () is execution
+//sayMyName op--> blank 
 
-// sayMyName()
 
-// function addTwoNumbers(number1, number2){
+/*
+ function addTwoNumbers1(number1, number2){  //here number1 and number2 are parameters
 
-//     console.log(number1 + number2);
-// }
+     console.log(number1 + number2); //console.log ke wajah se 8 print ho raha hein
+ }
+// addTwoNumbers1() //op--->NaN bcoz no argument were passed
+// addTwoNumbers1(3,4)//op--->7 //here 3 and 4 are Arguments
+// addTwoNumbers1(3,"4")//op-->34 (bcoz 3 is first converted into string and concatnated with 4)
+// addTwoNumbers1("4",3)//43
+// addTwoNumbers1(3,"a")//op-->3a
+// addTwoNumbers1(3,null)//op-->3 bcoz null is converted into 0 first and then 3+0= 3
+ const result1 = addTwoNumbers1(3, 5)//op-->8
+ console.log(result1)//op--->undefined because function has no returntype
+ /*
+ 8
+undefined
+*/
 
+
+
+/*
 function addTwoNumbers(number1, number2){
 
     // let result = number1 + number2
@@ -24,10 +48,14 @@ function addTwoNumbers(number1, number2){
 
 const result = addTwoNumbers(3, 5)
 
-// console.log("Result: ", result);
+console.log("Result: ", result); //op-->Result:  8 (yaha "Result: 8" ke pehele 8 isliye print nhi ho raha hein kyuki humne console ke bajay return likha)
+
+*/
 
 
-function loginUserMessage(username = "sam"){
+
+/*
+function loginUserMessage(username = "sam"){ //yaha humne default value set ki hein & thats why our program flow will never enter the if block 
     if(!username){
         console.log("PLease enter a username");
         return
@@ -35,15 +63,19 @@ function loginUserMessage(username = "sam"){
     return `${username} just logged in`
 }
 
-// console.log(loginUserMessage("hitesh"))
-// console.log(loginUserMessage("hitesh"))
+console.log(loginUserMessage())//op-->sam just logged in
+console.log(loginUserMessage("hitesh"))//op-->hitesh just logged in
+*/
 
 
-function calculateCartPrice(val1, val2, ...num1){
+
+
+
+function calculateCartPrice(val1, val2, ...num1){  
     return num1
 }
 
-// console.log(calculateCartPrice(200, 400, 500, 2000))
+console.log(calculateCartPrice(200, 400, 500, 2000))
 
 const user = {
     username: "hitesh",
