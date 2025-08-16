ccbb729808b351fddbf2f00957104b1cbaff8d78
SumanJana2 <420dominicontop@gmail.com>
2025-08-16 14:29:53 +0000

    xd

--- basics/switch.js
+++ basics/switch.js
@@ -1,4 +1,4 @@
-// switch (key) {
+// switch (key) {     //syntax of switch case statement
 //     case value:
         
 //         break;
@@ -12,7 +12,7 @@ const month = "march"
 switch (month) {
     case "jan":
         console.log("January");
-        break;
+        break; //agar break nhi likhuga toh ?,,aur yeh case match hota hein toh ?,,,yeh statement + aur uske baad ka sara code execute hoga except default case
     case "feb":
         console.log("feb");
         break;
@@ -26,4 +26,67 @@ switch (month) {
     default:
         console.log("default case match");
         break;
-}
+}//op-->march
+////--------------------------------------------------------------------------------------
+const hehe = "3"
+
+switch (hehe) {
+    case "1":
+        console.log("January");
+        break; //agar break nhi likhuga toh ?,,aur yeh case match hota hein toh ?,,,yeh statement + aur uske baad ka sara code execute hoga except default case
+    case "2":
+        console.log("feb");
+        break;
+    case "3":
+        console.log("march");
+        break;
+    case "4":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}//op-->march
+//----------------------------------------------------------------------------------
+const hehe2 = 3
+
+switch (hehe2) {
+    case "1":
+        console.log("January");
+        break; //agar break nhi likhuga toh ?,,aur yeh case match hota hein toh ?,,,yeh statement + aur uske baad ka sara code execute hoga except default case
+    case "2":
+        console.log("feb");
+        break;
+    case "3":
+        console.log("march");
+        break;
+    case "4":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}//op-->default case match
+//------------------------------------------------------------------------------------------------
+const hehe3 = "3"
+
+switch (hehe3) {
+    case 1:
+        console.log("January");
+        break; //agar break nhi likhuga toh ?,,aur yeh case match hota hein toh ?,,,yeh statement + aur uske baad ka sara code execute hoga except default case
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}//op-->default case match
